642518a934c3c1ff376f6feffc9e5f5147505a14
yoguimon <jhoninformatic4@gmail.com>
2024-07-14 19:59:00 -0400

    configuracion para todas las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import productos from '../views/apiProductos.vue'
 import carrito from '../views/carritoProductos.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes: [
     {
       path: '/productos',
