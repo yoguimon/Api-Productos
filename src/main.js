520d5ba83a5186c0b313240d2d1a081160c34944
yoguimon <jhoninformatic4@gmail.com>
2024-07-14 19:26:21 -0400

    nav eliminado de los productos

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-//import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 
 const app = createApp(App);
 
 app.use(router);
-//app.use(VueSweetalert2);
 app.mount('#app');
